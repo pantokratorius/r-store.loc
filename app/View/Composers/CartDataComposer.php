ef115a62805a33a02d8224b2b03dd275647800e4
Erik <eitu@mail.ru>
2024-01-15 18:21:26 +0300

    .

--- app/View/Composers/CartDataComposer.php
+++ app/View/Composers/CartDataComposer.php
@@ -9,13 +9,13 @@ class CartDataComposer
 {
     public function compose(View $view)
     {
+        $cart_price = 0;
        $cart = session()->get('cart', []);
        $cart_count = count($cart);
        
        if($cart){
-        $cart_price = 0; $cart_price = 0; 
         foreach($cart as $k=>$v){
-                $cart_price += str_replace('.','', $v['price']);
+                $cart_price += (int)str_replace('.','', $v['price']);
             }
         }
         $view->with('cart', $cart)
