6fb3eed2f110d03e78f81572518727d15df0a7f5
Erik <eitu@mail.ru>
2024-03-16 15:04:28 +0300

    .

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -39,7 +39,7 @@ class CheckoutController extends Controller
         $mail['date'] = 'Дата: '. $mail['date'];
         $mail['type'] = 'Оплата '. $type[ $mail['type']  ];
         $mail['delivery'] = $delivery[ $mail['delivery'] - 1 ];
-        $message = implode('<br>', $mail);
+        $message = implode(PHP_EOL, $mail);
         // dd($mail);
         
         $theme = 'Новый Заказ!';
