4b124559b9f6a6a3c5cfd7e94a9f27d0b24b825e
pantokratorius <eitu@mail.ru>
2024-03-18 22:52:41 +0300

    .

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -45,11 +45,11 @@ class CheckoutController extends Controller
         $mail['date'] = 'Дата: '. $mail['date'];
         $mail['type'] = 'Оплата '. $type[ $mail['type']  ];
         $mail['delivery'] = $delivery[ $mail['delivery'] - 1 ];
-        $message = implode(PHP_EOL, $mail);
+        // $message = implode(PHP_EOL, $mail);
         // dd($mail);
         $recepient = 'erik.krasnauskas@yandex.ru';
 
-        $res = Mail::to($recepient)->send(new OrderSend($message));
+        $res = Mail::to($recepient)->send(new OrderSend($mail));
         return $res;
     }
 
