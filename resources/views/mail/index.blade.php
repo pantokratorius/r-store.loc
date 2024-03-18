d14e32ab8a31f661a82491d005049d1832b5614b
pantokratorius <eitu@mail.ru>
2024-03-18 23:20:05 +0300

    .

--- resources/views/mail/index.blade.php
+++ resources/views/mail/index.blade.php
@@ -9,24 +9,21 @@
         @if(!is_array($val))
             <p>{{ $val }}</p>
         @else
-
+           <table border=0 >
             @foreach ($val as $v)
-            <div class="co-basket_item-image_container">
-                <div class="co-basket_item-image">
-                   <img style="max-width: 150px" @if(isset($v['image']) )
+            <tr>
+                <td>
+                   <img style="max-width: 150px"
                       src="{{ asset('images/products/' . $v['image']) }}" alt="{{$v['real_item_name']}}" title="{{$v['real_item_name']}}"
-                      @else
-                      src="{{url('/images/noimage.png')}}" alt="Apple product" title="" style="max-width: 100px; object-fit: contain"
-                  @endif />
-                </div>
-              </div>
-              <div class="co-basket_item-description">{{ $v['real_item_name'] }}</div>
-              <div class="co-basket_item-total">
-                <span class="co-basket_item-count">{{ $v['quantity'] }}</span> x <span class="co-basket_item-price co-price--current">{{ $v['price'] }}&nbsp;руб</span>
-              </div>
-            </div>
+                      />
+                </td>
+              <td >{{ $v['real_item_name'] }}</td>
+              <td>
+                <span>{{ $v['quantity'] }}</span> x <span>{{ $v['price'] }}&nbsp;руб</span>
+              </td>
+        </tr>
             @endforeach
-
+        </table>
         @endif
     @endforeach
 
