5eeb7776de3c2421f26fbfe362e612f8d1882b2c
Erik <eitu@mail.ru>
2024-03-21 14:16:00 +0300

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+
+        $schedule->command('sitemap:generate')->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
