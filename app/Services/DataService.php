254b6c996eda8ac0730d6c473ccc377bd061a1da
Erik <eitu@mail.ru>
2024-04-01 17:58:58 +0300

    .

--- app/Services/DataService.php
+++ app/Services/DataService.php
@@ -38,7 +38,7 @@ class DataService {
                 if($key != 'real_name'){
                     $transfer[$k][$key]['price'] = isset($new_nacenka[$key]) ? self::formatNumber($new_nacenka[$key]) : self::formatNumber(  (int)str_replace('.','',$val['price'])  + $nacenk );
                     foreach($names_my as $kk => $nam){ //dump([$nam, $k]);
-                        if(stripos($k, $nam) !==false){
+                        if(stripos($k, $nam) !==false && stripos($val['real_name'], $nam) === false){
                             $transfer[$k][$key]['real_item_name'] = $this->getName( (isset($fullName[$kk]) ? $fullName[$kk] : $nam) . ' ' .EmojiRemover::filter( $val['real_name']));
                             break;
                         }
