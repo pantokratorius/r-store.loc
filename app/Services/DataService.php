462ed65c2313ccd7a8b6ad296fc5a8bdd4ba6feb
Erik <eitu@mail.ru>
2024-09-13 17:41:18 +0300

    .

--- app/Services/DataService.php
+++ app/Services/DataService.php
@@ -11,7 +11,7 @@ class DataService {
 
     public function getAllData(){
 
-        $names_my = ['Iphone', 'Watch'];
+        $names_my = ['iphone', 'Watch'];
         $fullName = [1 => 'Apple Watch'];
         // $names_my = ['aaaaaaaaa'];
         // $fullName = [];
