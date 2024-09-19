964e9609090df4058d074b5836c7ddfb593f88ff
Erik <eitu@mail.ru>
2024-09-19 14:27:21 +0300

    .

--- app/Services/DataService.php
+++ app/Services/DataService.php
@@ -13,6 +13,8 @@ class DataService {
 
         $names_my = ['Iphone', 'Watch'];
         $fullName = [1 => 'Apple Watch'];
+        $replace_arr = ['Sim-eSim', 'iPhone', 'Dual Sim'];
+
         // $names_my = ['aaaaaaaaa'];
         // $fullName = [];
 
@@ -38,8 +40,13 @@ class DataService {
                 // $key = EmojiRemover::filter($key);
                 $key = str_replace(['/', '-'],'',$key);
                 if($key != 'real_name'){
+                    
+                    $val['real_name'] = str_replace($replace_arr, '', $val['real_name']);
+
                     $transfer[$k][$key]['price'] = isset($new_nacenka[$key]) ? self::formatNumber($new_nacenka[$key]) : self::formatNumber(  (int)str_replace('.','',$val['price'])  + $nacenk );
                     foreach($names_my as $kk => $nam){ //dump([$nam, $k]);
+
+
                         if(stripos($k, $nam) !==false){
                             $transfer[$k][$key]['real_item_name_ru'] = ($em = EmojiRemover::getEmoji( $val['real_name']) ) . ' ' . $this->getName( (isset($fullName[$kk]) ? $fullName[$kk] : $nam .  str_replace($em, '', $val['real_name'] )) ) ; 
                             $transfer[$k][$key]['real_item_name'] = ($em = EmojiRemover::getEmoji( $val['real_name']) ) . ' ' .  (isset($fullName[$kk]) ? $fullName[$kk] : $nam .  str_replace($em, '', $val['real_name'] )) ; 
@@ -60,6 +67,9 @@ class DataService {
                 // $key = EmojiRemover::filter($key);
                 $key = str_replace(['/', '-'],'',$key);
                     if($key != 'real_name'){
+
+                        $val['real_name'] = str_replace($replace_arr, '', $val['real_name']);
+
                         $transfer[$k][$key]['price'] =  isset($new_nacenka[$key]) ? self::formatNumber($new_nacenka[$key]) : self::formatNumber( (int)str_replace('.','',$val['price'] ) + 5000 );    //save([$res[$k],  $val ]);
 
                         foreach($names_my as $kk => $nam){//dump($names_my);
@@ -89,7 +99,7 @@ class DataService {
 
 // dump($name);
 
-
+     
 
         $replace = [
             'Black' => '"тёмная ночь"',
